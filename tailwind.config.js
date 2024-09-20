e1eabdcf2ded43f46ad0668cd659513b929b3584
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-09-20 08:16:51 +0600

    intial setup done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customBlue: {
+            100: '#d7f1ff',
+            200: '#b8e8ff',
+            300: '#87dcff',
+            400: '#4fc6ff',
+            500: '#26a8ff',
+            600: '#0f8bff',
+            700: '#0874f2',
+            800: '#0e5abf',
+            900: '#124e96',
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
