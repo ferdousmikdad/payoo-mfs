51416c98ead75f04d75e30e2e8357cfd5acf73a1
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-09-20 12:49:09 +0600

    Just try to move to the login page through logout button (funly not proper way)

---
+++ js/logout.js
@@ -0,0 +1,7 @@
+
+// it's not the proper way. I just to try to move to the login page
+
+document.getElementById('logout').addEventListener('click', function(){
+    // console.log('click the logout button')
+    window.location.href = './index.html';
+})
