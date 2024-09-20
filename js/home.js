729146093c115564626342a44404fdac3465c320
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-09-21 02:32:30 +0600

    Verify pin number and add money to the main balance

---
+++ js/home.js
@@ -0,0 +1,32 @@
+// Add money to the account
+
+// step-1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money')
+    .addEventListener('click' , function(event){
+    event.preventDefault();
+
+    // Step-2: get money to be added to the account balance
+    const inputAddMoney = document.getElementById('input-add-money').value;
+
+    // Step-3: Pin number validation
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    // Verify the pin number (not ideal way just for fun)
+    if(inputPinNumber === 'admin'){
+
+        // step-4: get the current balance
+        const accountBalance = document.getElementById('account-balance').innerText;
+        // step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(inputAddMoney);
+        const balanceNumber = parseFloat(accountBalance);
+        const newBalance = addMoneyNumber + balanceNumber;
+
+        console.log(newBalance);
+
+        // step-6: update the balance in the DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else{
+        alert('Failed to add money');
+    }
+})
