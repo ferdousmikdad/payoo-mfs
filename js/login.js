5bec93cd69d94576cc687b51645e8fff043c4cfc
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-09-20 09:08:59 +0600

    Added login event handler and prevent form default behavior

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+// document.getElementById('id').addEventListener('click', function(){})
+
+// TODO: search: form submit reloading the page
+
+// step-1 set event handler
+document.getElementById('btn-login')
+    .addEventListener('click', function(event){
+        
+        // step-2 prevent default behavior (prevent reloading browser)
+        event.preventDefault(); //it will remove the default page reloading behavior
+
+        // step-3 get the mobile number
+        const mobileNumber = document.getElementById('mobile-number').value;
+        // console.log(mobileNumber);
+
+
+    });
