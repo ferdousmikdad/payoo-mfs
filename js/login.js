1d026b7eb3c9e2959875d804e5d1af39c4471459
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-09-20 09:36:35 +0600

    Handle Login and redirect to the home page

--- js/login.js
+++ js/login.js
@@ -3,16 +3,27 @@
 
 // TODO: search: form submit reloading the page
 
-// step-1 set event handler
+// step-1: set event handler
 document.getElementById('btn-login')
     .addEventListener('click', function(event){
         
-        // step-2 prevent default behavior (prevent reloading browser)
+        // step-2: prevent default behavior (prevent reloading browser)
         event.preventDefault(); //it will remove the default page reloading behavior
 
-        // step-3 get the mobile number
+        // step-3: get the mobile number and pin number
         const mobileNumber = document.getElementById('mobile-number').value;
-        // console.log(mobileNumber);
+        const pinNumber = document.getElementById('pin-number').value;
 
+        // console.log(mobileNumber, pinNumber);
 
+        // step-4: validate mobile and pin number
+        // this is the temporary (bad) way
+        if(mobileNumber === '5' && pinNumber === '123'){
+            console.log('You are logged in');
+            // step-5: allow user to website 
+            window.location.href = './home.html';
+        }
+        else{
+            alert('Wrong mobile or pin number');
+        }
     });
