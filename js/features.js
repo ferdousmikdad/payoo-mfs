952632bec15dfcc508e3a6a1e43aa08487188684
Ferdous Mikdad <mikdadtaqi2024@gmail.com>
2024-09-21 15:55:18 +0600

    Creat resuable function and features toggle button

--- js/features.js
+++ js/features.js
@@ -1,20 +1,15 @@
-// Show the cash out form
-document.getElementById('btn-show-cash-out')
-.addEventListener('click' , function(){
-
-    // show cash out button
-    document.getElementById('cash-out-form').classList.remove('hidden');
-
-    // Hide the add money form
-    document.getElementById('add-money-form').classList.add('hidden');
-})
-
 document.getElementById('btn-show-add-money')
-.addEventListener('click' , function(){
+    .addEventListener('click', function(){
+        showSectionById('add-money-form');
+});
 
-    // show the Add money form
-    document.getElementById('add-money-form').classList.remove('hidden');
+document.getElementById('btn-show-cash-out')
+    .addEventListener('click', function(){
+        showSectionById('cash-out-form');
+});
 
-    // hide the cash out form
-    document.getElementById('cash-out-form').classList.add('hidden');
-})
+
+document.getElementById('btn-show-transactions')
+    .addEventListener('click', function(){
+        showSectionById('transaction-section');
+});
---
+++ js/utilities.js
@@ -0,0 +1,26 @@
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+
+    return inputNumber;
+
+}
+
+// Main Balance 
+function getTextFieldValueById (id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+}
+
+// Show section
+function showSectionById (id){
+    // hid all the sections
+    document.getElementById('add-money-form').classList.add('hidden');
+    document.getElementById('cash-out-form').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+
+    // show the section with the provided id
+    document.getElementById(id).classList.remove('hidden');
+
+}
